e281e7a2cc45b11ef0ea9bff9a7f592144acd263
Jim <james.leitch@mulliongroup.com>
2020-05-13 15:24:56 +1000

    fox test modules
    + more dynamic
    + reduced to one
    
    Signed-off-by: leitchy <jim@leitches.com>

--- Source/moja.flint/include/moja/flint/testmodule.h
+++ Source/moja.flint/include/moja/flint/testmodule.h
@@ -1,5 +1,5 @@
-#ifndef MOJA_FLINT_TESTMODULE2_H_
-#define MOJA_FLINT_TESTMODULE2_H_
+#ifndef MOJA_FLINT_TESTMODULE1_H_
+#define MOJA_FLINT_TESTMODULE1_H_
 
 #include "moja/flint/ioperationresult.h"
 #include "moja/flint/ipool.h"
@@ -10,11 +10,11 @@ namespace flint {
 
 // --------------------------------------------------------------------------------------------
 
-class FLINT_API TestModule2 : public ModuleBase {
+class FLINT_API TestModule : public ModuleBase {
   public:
    // _pool1(nullptr), _pool2(nullptr), _pool3(nullptr), _variable1(nullptr), _variable2(nullptr), _variable3(nullptr)
-   TestModule2() = default;
-   virtual ~TestModule2() = default;
+   TestModule() = default;
+   virtual ~TestModule() = default;
 
    void configure(const DynamicObject& config) override;
    void subscribe(NotificationCenter& notificationCenter) override;
@@ -38,8 +38,16 @@ class FLINT_API TestModule2 : public ModuleBase {
    double ratio_1;
    double ratio_2;
    double ratio_3;
+
+   std::string variable_1;
+   std::string variable_2;
+   std::string variable_3;
+
+   std::string pool_1;
+   std::string pool_2;
+   std::string pool_3;
 };
 
 }  // namespace flint
 }  // namespace moja
-#endif  // MOJA_FLINT_TESTMODULE2_H_
+#endif  // MOJA_FLINT_TESTMODULE1_H_
---
+++ Source/moja.flint/src/testmodule.cpp
@@ -0,0 +1,92 @@
+#include "moja/flint/testmodule.h"
+
+#include "moja/flint/ilandunitdatawrapper.h"
+#include "moja/flint/ioperation.h"
+#include "moja/flint/ivariable.h"
+
+#include <moja/notificationcenter.h>
+#include <moja/signals.h>
+
+namespace moja {
+namespace flint {
+
+// --------------------------------------------------------------------------------------------
+
+void TestModule::configure(const DynamicObject& config) {
+   ratio_1 = 0.50;
+   ratio_2 = 0.50;
+   ratio_3 = 0.50;
+
+   variable_1 = "variable 1";
+   variable_2 = "variable 2";
+   variable_3 = "variable 3";
+
+   pool_1 = "Pool 1";
+   pool_2 = "Pool 2";
+   pool_3 = "Pool 3";
+
+   if (config.contains("ratio_1")) {
+      ratio_1 = config["ratio_1"];
+   }
+   if (config.contains("ratio_2")) {
+      ratio_3 = config["ratio_2"];
+   }
+   if (config.contains("ratio_3")) {
+      ratio_3 = config["ratio_3"];
+   }
+
+   if (config.contains("variable_1")) {
+      variable_1 = config["variable_1"].extract<const std::string>();
+   }
+   if (config.contains("variable_2")) {
+      variable_2 = config["variable_2"].extract<const std::string>();
+   }
+   if (config.contains("variable_3")) {
+      variable_3 = config["variable_3"].extract<const std::string>();
+   }
+
+     if (config.contains("pool_1")) {
+      pool_1 = config["pool_1"].extract<const std::string>();
+   }
+   if (config.contains("pool_2")) {
+      pool_2 = config["pool_2"].extract<const std::string>();
+   }
+   if (config.contains("pool_3")) {
+      pool_3 = config["pool_3"].extract<const std::string>();
+   }
+
+}
+
+// --------------------------------------------------------------------------------------------
+
+void TestModule::subscribe(NotificationCenter& notificationCenter) {
+   notificationCenter.subscribe(signals::LocalDomainInit, &TestModule::onLocalDomainInit, *this);
+   notificationCenter.subscribe(signals::TimingInit, &TestModule::onTimingInit, *this);
+   notificationCenter.subscribe(signals::TimingStep, &TestModule::onTimingStep, *this);
+}
+
+void TestModule::onLocalDomainInit() {
+   // Pools
+   _pool1 = _landUnitData->getPool(pool_1);
+   _pool2 = _landUnitData->getPool(pool_2);
+   _pool3 = _landUnitData->getPool(pool_3);
+
+   // Variables
+   _variable1 = _landUnitData->getVariable(variable_1);
+   _variable2 = _landUnitData->getVariable(variable_2);
+   _variable3 = _landUnitData->getVariable(variable_3);
+}
+
+void TestModule::onTimingInit() {}
+
+void TestModule::onTimingStep() {
+   auto operation = _landUnitData->createProportionalOperation();
+   operation->addTransfer(_pool1, _pool2, ratio_1)
+       ->addTransfer(_pool2, _pool3, ratio_2)
+       ->addTransfer(_pool3, _pool1, ratio_3);
+
+   _landUnitData->submitOperation(operation);
+}
+
+}  // namespace flint
+}  // namespace moja
