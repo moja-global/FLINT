ecb56df55b0981a670b574dad461ff8e75ea3216
parth-gr <50455445+parth-gr@users.noreply.github.com>
2020-08-30 15:30:48 +0530

    Updated dynamic to std::any in writesystemconfig
    Signed-off-by: parth-gr <50455445+parth-gr@users.noreply.github.com>

--- Source/moja.flint/src/writesystemconfig.cpp
+++ Source/moja.flint/src/writesystemconfig.cpp
@@ -35,11 +35,11 @@ namespace flint {
 // --------------------------------------------------------------------------------------------
 
 void WriteSystemConfig::configure(const DynamicObject& config) {
-   _name = config["name"].convert<std::string>();
-   _outputPath = config.contains("output_path") ? config["output_path"].convert<std::string>() : "";
-   _writeFrequency = config.contains("write_frequency") ? config["write_frequency"].convert<UInt32>() : 0;
+   _name = config["name"].extract<std::string>();
+   _outputPath = config.contains("output_path") ? config["output_path"].extract<std::string>() : "";
+   _writeFrequency = config.contains("write_frequency") ? config["write_frequency"].extract<UInt32>() : 0;
    _writeOutstepFrequency =
-       config.contains("write_outstep_frequency") ? config["write_outstep_frequency"].convert<UInt32>() : 0;
+       config.contains("write_outstep_frequency") ? config["write_outstep_frequency"].extract<UInt32>() : 0;
 
    // set all notifications to false
    for (auto& val : _onNotificationArray) val = false;
@@ -49,11 +49,11 @@ void WriteSystemConfig::configure(const DynamicObject& config) {
       if (dynamic.isVector()) {
          const auto& arr = dynamic.extract<const DynamicVector>();
          for (auto& val : arr) {
-            auto notStr = val.convert<std::string>();
+            auto notStr = val.extract<std::string>();
             _onNotificationArray[convertNotificationStringToIndex(notStr)] = true;
          }
       } else {
-         _onNotificationArray[convertNotificationStringToIndex(config["on_notification"].convert<std::string>())] =
+         _onNotificationArray[convertNotificationStringToIndex(config["on_notification"].extract<std::string>())] =
              true;
       }
    } else {
